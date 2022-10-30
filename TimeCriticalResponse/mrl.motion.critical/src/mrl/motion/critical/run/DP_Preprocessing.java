37ff19a6eebc4f5f22bb87e04134fbfa3efe403c
Seungjun Park(박승준) <77137245+seungjunpark-khu@users.noreply.github.com>
2022-10-30 11:32:33 +0900

    modify .java for duel configs

--- TimeCriticalResponse/mrl.motion.critical/src/mrl/motion/critical/run/DP_Preprocessing.java
+++ TimeCriticalResponse/mrl.motion.critical/src/mrl/motion/critical/run/DP_Preprocessing.java
@@ -5,13 +5,20 @@ import mrl.motion.neural.agility.match.TransitionDPGenerator;
 public class DP_Preprocessing {
 
 	public static void main(String[] args) {
-		String[] actions = MartialArtsConfig.actionTypes;
+//		String[] actions = MartialArtsConfig.actionTypes;
+//		int cLabelSize = MartialArtsConfig.LOCO_ACTION_SIZE;
+//		
+//		String dataFolder = "martial_arts_compact";
+//		String tPoseFile = "data\\t_pose_ue2.bvh";
 		// locomotion action size(cyclic actions)
-		int cLabelSize = MartialArtsConfig.LOCO_ACTION_SIZE;
-		
-		String dataFolder = "martial_arts_compact";
-		String tPoseFile = "data\\t_pose_ue2.bvh";
 //		TransitionDPGenerator.printDistribution = false;
+		
+		String[] actions = DuelConfig.actionTypes;
+		int cLabelSize = DuelConfig.LOCO_ACTION_SIZE;
+		
+		String dataFolder = "duel";
+		String tPoseFile = "data\\stop_fencing.bvh";
+		
 		TransitionDPGenerator.make(dataFolder, tPoseFile, actions, cLabelSize);
 	}
 }
