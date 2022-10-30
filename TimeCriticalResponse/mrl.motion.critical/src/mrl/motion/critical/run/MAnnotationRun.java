446eb1fcff510380fb7a7595499317ea0dc3ab89
Seungjun Park(박승준) <77137245+seungjunpark-khu@users.noreply.github.com>
2022-10-30 11:29:06 +0900

    add duel config & remove old dataset

--- TimeCriticalResponse/mrl.motion.critical/src/mrl/motion/critical/run/MAnnotationRun.java
+++ TimeCriticalResponse/mrl.motion.critical/src/mrl/motion/critical/run/MAnnotationRun.java
@@ -2,12 +2,17 @@ package mrl.motion.critical.run;
 
 import mrl.motion.annotation.MotionAnnotationRun;
 
+/**
+ * @author user
+ *
+ */
 public class MAnnotationRun {
 
 	public static void main(String[] args) {
-		String dataFolder = "data\\martial_arts_compact";
+		//String dataFolder = "data\\martial_arts_compact";
+		String dataFolder = "data\\duel";
 		
-		boolean openTransition = false;
+		boolean openTransition = true;
 //		openTransition = true;
 		MotionAnnotationRun.open(dataFolder, !openTransition, true);
 	}
