103a9d643cc3dba502fd881f5e265145173ed4db
Eunseop Shim <lumeriith@gmail.com>
2022-11-06 11:14:06 +0900

    Calibrate animation retargeting parameters

--- TheDuel/Assets/Scripts/Networking/Visualizers/ModelDataCharacterVisualizer.cs
+++ TheDuel/Assets/Scripts/Networking/Visualizers/ModelDataCharacterVisualizer.cs
@@ -14,15 +14,12 @@ public class ModelDataCharacterVisualizer : ModelVisualizerBase
         [HorizontalGroup("A"), HideLabel]
         public HumanBodyBones bone;
         public Quaternion baseRot;
-        public Quaternion afterRot;
     }
 
     [ListDrawerSettings()]
     public List<BoneItem> skeletonSettings;
     public Animator connectedCharacter;
     public bool enableRetargeting;
-    public bool useBaseRot;
-    public bool useAfterRot;
     public bool visualizeIntermediateNode;
     public float bodyScale = 0.1f;
     public float translationScale = 0.01f;
@@ -51,36 +48,11 @@ public class ModelDataCharacterVisualizer : ModelVisualizerBase
             var fromRotation = modelTransform.rotation;
             var toRotation = characterBone.rotation;
             
-            // afterRot * fromRotation = toRotation;
-            // OR fromRotation * baseRot = toRotation;
-            item.afterRot = toRotation * Quaternion.Inverse(fromRotation);
             item.baseRot = Quaternion.Inverse(fromRotation) * toRotation;
         }
     }
 
-    [Button]
-    private void BuildUsingJointTranslation()
-    {
-        for (int i = 0; i < skeletonSettings.Count; i++)
-        {
-            var item = skeletonSettings[i];
-            var characterBone = connectedCharacter.GetBoneTransform(item.bone);
-            var modelTransform = _worldSpaceBoneTransforms[item.name];
 
-            var fromTransition = setup.jointByBoneName[item.name].translation;
-            var toTransition = characterBone.localPosition;
-
-  
-            
-    
-            // afterRot * fromRotation = toRotation;
-            // OR fromRotation * baseRot = toRotation;
-            item.afterRot = Quaternion.Inverse(Quaternion.FromToRotation(fromTransition, toTransition));
-            item.baseRot = Quaternion.Inverse(Quaternion.FromToRotation(fromTransition, toTransition));
-        }
-    }
-    
-    
 
     protected override void OnDataChanged()
     {
@@ -116,9 +88,7 @@ public class ModelDataCharacterVisualizer : ModelVisualizerBase
                     if (pair.name == "Hips")
                         boneTransform.position = tr.position;
                     boneTransform.rotation = tr.rotation;
-                    if (useBaseRot) boneTransform.rotation = boneTransform.rotation * pair.baseRot;
-                    if (useAfterRot)
-                        boneTransform.rotation = pair.afterRot * boneTransform.rotation;
+                    boneTransform.rotation = boneTransform.rotation * pair.baseRot;
                 }
             }
         }
