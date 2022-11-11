38a685a8f83c922338a5511764250ac0e80a7b01
tngksdlstk12 <ksh42734739@gmail.com>
2022-11-12 00:47:32 +0900

    add CarryingMan Prefab

---
+++ TheDuel/Assets/Res/MapAssets/Scripts/CarryingMan.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryingMan : MonoBehaviour
+{
+    [SerializeField] private GameObject carryingMan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    
+    void Move()
+    {
+        carryingMan.transform.Translate(new Vector3(0, 0, 0.5f) * Time.deltaTime);
+    }
+}
