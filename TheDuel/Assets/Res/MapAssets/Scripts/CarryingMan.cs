c7d3009210765039898766484f6470863e2ebf4e
tngksdlstk12 <ksh42734739@gmail.com>
2022-11-18 00:52:22 +0900

    add ScoreSystem,  Staff System

--- TheDuel/Assets/Res/MapAssets/Scripts/CarryingMan.cs
+++ TheDuel/Assets/Res/MapAssets/Scripts/CarryingMan.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CarryingMan : MonoBehaviour
 {
     [SerializeField] private GameObject carryingMan;
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private GameObject startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(carryingMan, 100f);
+        StartCoroutine(MoveCycle());
     }
 
     // Update is called once per frame
@@ -17,9 +19,30 @@ public class CarryingMan : MonoBehaviour
     {
         Move();
     }
-    
+
     void Move()
     {
-        carryingMan.transform.Translate(new Vector3(0, 0, 0.5f) * Time.deltaTime);
+        carryingMan.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+    }
+
+    IEnumerator MoveCycle()
+    {
+        yield return new WaitForSeconds(75.0f);
+        StartCoroutine(RandomStart());
+
+    }
+    IEnumerator RandomStart()
+    {
+        yield return new WaitForSeconds(3.0f);
+        int randomint = Random.Range(0, 3);
+        if (randomint == 0) // 1/3 È®·ü
+        {
+            carryingMan.transform.position = startPosition.transform.position;
+            StartCoroutine(MoveCycle());
+        }
+        else
+        {
+            StartCoroutine(RandomStart());
+        }
     }
 }
