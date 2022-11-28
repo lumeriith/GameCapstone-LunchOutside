403032f229d9d999d8b7da94d25257e2d6353234
Eunseop Shim <lumeriith@gmail.com>
2022-11-28 15:45:21 +0900

    Fix something

--- TheDuel/Assets/Res/MapAssets/Scripts/WeaponBox.cs
+++ TheDuel/Assets/Res/MapAssets/Scripts/WeaponBox.cs
@@ -4,80 +4,18 @@ using UnityEngine;
 
 public class WeaponBox : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] itemsPool;
 
-    public GameObject thisObject;
-    [SerializeField] GameObject mFork;
-    [SerializeField] GameObject mGrenade;
-    [SerializeField] GameObject mStone;
+    private int _health = 2;
 
-    private int mItemType;
-    private int mHp = 0;
-
-    private void Awake()
+    public void Hit()
     {
-        mHp = 2;
-        mItemType = Random.Range(0, 3);
-        Debug.Log("init");
-        Debug.Log("Item type: " + mItemType);
-    }
-
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        Debug.Log("Collision");
-
-        mHp -= 1;
-
-        GameObject obj;
-        Transform tmp = new GameObject().GetComponent<Transform>();
-        tmp.position = new Vector3(-3.17f, 1.0f, -0.83f);
-
-        if (mHp <= 0)
+        _health--;
+        
+        if (_health <= 0)
         {
-            switch (mItemType)
-            {
-                case 0:
-                    obj = Instantiate(mFork, tmp);
-                    Debug.Log("Create fork");
-                    break;
-
-                case 1:
-                    obj = Instantiate(mGrenade, tmp);
-                    Debug.Log("Create grenade");
-                    break;
-
-                case 2:
-                    obj = Instantiate(mStone, tmp);
-                    Debug.Log("Create stone");
-                    break;
-            }
-            Destroy(thisObject);
-            Debug.Log("Destroy box");
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        //Debug.Log("fdsdf");
-        if (other.gameObject.name=="Player")
-        {
-            if (Input.GetKeyDown("e"))
-            {
-                Debug.Log("WeaponBoxTest");
-                Instantiate(mFork, thisObject.transform); //ÀÌ°Å ¿Ö ¾È µÅ
-                Destroy(thisObject);
-            }
+            Instantiate(itemsPool[Random.Range(0, itemsPool.Length)], transform.position, transform.rotation);
+            Destroy(gameObject);
         }
     }
 }
